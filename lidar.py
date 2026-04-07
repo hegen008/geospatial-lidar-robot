a51faf8ab69ae1558863620330aecd541f9922ee
Arlan Hegenbarth <aohegenbarth@gmail.com>
2026-04-08 02:16:54 +0800

    Add files via upload

--- lidar.py
+++ lidar.py
@@ -5,21 +5,22 @@ import numpy as np
 
 class LidarRecording:
     # Initialize lidar class with port
-    def __init__(self, port='r/dev/ttyUSB0'): # Windows port r'\\.\COM3'
+    def __init__(self, port='/dev/ttyUSB0'): # Windows port r'\\.\COM3'
         # Save attribute
         self.start_time = None
         self.end_time = None
         self.port = port
         self.thread = None
         self.recording = False
+        self.used = False       # True when this instance has recorded some data
 
         # Establish data structures
-        self.angle_data = []
-        self.dist_data = []
-        self.time_data = []
+        self.angle_data = []        # degrees
+        self.dist_data = []         # meters
+        self.time_data = np.array([]) # seconds
         
         # establish lidar collection
-        self.lidar = RPLidar(self.port) # For Windows
+        self.lidar = RPLidar(self.port)
     
     # Internal method to run data collection on its own thread
     def _collect_data(self):
@@ -32,7 +33,7 @@ class LidarRecording:
                 if not self.recording:
                     break
                 for quality, angle, distance in scan:
-                    if quality > 0:
+                    if quality > 0:     # when quality = 0, this means that the data is bad and should be ignored
                         self.dist_data.append(distance / 1000) # Adjust to meters
                         self.angle_data.append(angle)
 
@@ -47,7 +48,9 @@ class LidarRecording:
 
     # Start data recording
     def start(self):
-        if not self.recording:
+        if self.used:
+            print('This instance has already recorded data. Please create a new instance to record new data.')
+        elif not self.recording:
             self.recording = True
             self.thread = threading.Thread(target=self._collect_data)
             self.thread.start()
@@ -59,5 +62,10 @@ class LidarRecording:
             if self.thread:
                 self.thread.join()
 
+            self.dist_data = np.array(self.dist_data)
+            self.angle_data = np.array(self.angle_data)
+
             # Interpolate times
-            self.time_data += np.linspace(self.start_time, self.end_time, num=len(self.dist_data))
+            self.time_data = np.linspace(0, (self.end_time - self.start_time), num=len(self.dist_data))
+
+        self.used = True
